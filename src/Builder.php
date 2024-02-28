c66e9f77215b25898bbc653b585b94c0a67c797e
enricodelazzari <enrico.delazzari@maize.io>
2024-02-28 19:34:25 +0100

    wip

--- src/Builder.php
+++ src/Builder.php
@@ -2,6 +2,46 @@
 
 namespace Maize\RulesBuilder;
 
+use Closure;
+use Illuminate\Support\Arr;
+
 class Builder
 {
+    public function __construct(
+        private array $rules = []
+    ) {
+    }
+
+    public static function new(): self
+    {
+        return new self();
+    }
+
+    public function add(
+        array|string $attributes,
+        Closure|array|string $rules = []
+    ): self {
+
+        $attributes = Arr::wrap($attributes);
+
+        foreach ($attributes as $key => $value) {
+            $attribute = array_is_list($attributes)
+                ? $value
+                : $key;
+
+            $this->rules[$attribute] = match (true) {
+                $rules instanceof Closure => array_is_list($attributes)
+                    ? $rules()
+                    : $rules($value),
+                default => $rules,
+            };
+        }
+
+        return $this;
+    }
+
+    public function get(): array
+    {
+        return $this->rules;
+    }
 }
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,78 @@
 <?php
 
-it('can test', function () {
-    expect(true)->toBeTrue();
+use Maize\RulesBuilder\Builder;
+
+it('can add rules from string', function () {
+    $rules = Builder::new()
+        ->add('title', 'required|string|max:20')
+        ->get();
+
+    expect($rules)->toBe([
+        'title' => 'required|string|max:20',
+    ]);
+});
+
+it('can add rules from array', function () {
+    $rules = Builder::new()
+        ->add('title', ['required', 'string', 'max:20'])
+        ->get();
+
+    expect($rules)->toBe([
+        'title' => ['required', 'string', 'max:20'],
+    ]);
+});
+
+it('can add multiple rules', function () {
+    $rules = Builder::new()
+        ->add('title', 'required|string|max:20')
+        ->add('description', ['required', 'string', 'max:1000'])
+        ->get();
+
+    expect($rules)->toBe([
+        'title' => 'required|string|max:20',
+        'description' => ['required', 'string', 'max:1000'],
+    ]);
+});
+
+it('can add rules for multiple attributes', function () {
+    $rules = Builder::new()
+        ->add(['title', 'description'], 'required|string')
+        ->get();
+
+    expect($rules)->toBe([
+        'title' => 'required|string',
+        'description' => 'required|string',
+    ]);
+});
+
+it('can add rules from string for multiple attributes using an associative array', function () {
+    $rules = Builder::new()
+        ->add([
+            'title' => 'max:20',
+            'description' => 'max:1000',
+        ], fn ($rule) => "required|string|{$rule}")
+        ->get();
+
+    expect($rules)->toBe([
+        'title' => 'required|string|max:20',
+        'description' => 'required|string|max:1000',
+    ]);
+});
+
+it('can add rules from array for multiple attributes using an associative array', function () {
+    $rules = Builder::new()
+        ->add([
+            'title' => 'max:20',
+            'description' => 'max:1000',
+        ], fn ($rule) => [
+            'required',
+            'string',
+            $rule,
+        ])
+        ->get();
+
+    expect($rules)->toBe([
+        'title' => ['required', 'string', 'max:20'],
+        'description' => ['required', 'string', 'max:1000'],
+    ]);
 });
